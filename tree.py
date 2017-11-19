b510025025227b26dd89ab37415d6907b83bcd0f
PrimoWW <992462276@qq.com>
2017-11-19 14:47:23 +0800

    添加中序和后序遍历

--- tree.py
+++ tree.py
@@ -31,6 +31,7 @@ class Tree():
                     q.append(pop_node.right_child)
 
     def pre_order(self, root):
+        # 前序遍历，注意return中3者的位置关系
         if root is None:
             return []
         result = [root.item]
@@ -38,7 +39,26 @@ class Tree():
         right_item = self.pre_order(root.right_child)
         return result + left_item + right_item
 
+    def in_order(self, root):
+        # 中序遍历，注意return中3者的位置关系
+        if root is None:
+            return []
+        result = [root.item]
+        left_item = self.in_order(root.left_child)
+        right_item = self.in_order(root.right_child)
+        return left_item + result + right_item
+
+    def post_order(self, root):
+        # 后序遍历，注意return中3者的位置关系
+        if root is None:
+            return []
+        result = [root.item]
+        left_item = self.post_order(root.left_child)
+        right_item = self.post_order(root.right_child)
+        return left_item + right_item + result
+
     def exchange(self, root):
+        # 交换左右子树
         if root is None:
             return []
         if root.left_child is None and root.right_child is None:
@@ -57,5 +77,7 @@ t.add('E')
 t.add('F')
 t.add('G')
 print(t.pre_order(t.root))
+print(t.in_order(t.root))
+print(t.post_order(t.root))
 t.exchange(t.root)
 print(t.pre_order(t.root ))

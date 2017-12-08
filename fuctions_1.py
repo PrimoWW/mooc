7ea3b2b968e84e6492bf8f8f6aac38b3ab3572bd
PrimoWW <992462276@qq.com>
2017-12-08 23:06:25 +0800

    创建并测试一个函数，然后读取__doc__属性，在检查他的类型

--- fuctions_1.py
+++ fuctions_1.py
@@ -1,3 +1,6 @@
+"""
+创建并测试一个函数，然后读取__doc__属性，在检查他的类型
+"""
 def factorial(n):
     """this function is used to find n**(n-1)**(n-2)**...**1"""
     return 1 if n==1 else n * factorial(n-1)
@@ -5,3 +8,4 @@ def factorial(n):
 if __name__=="__main__":
     print(factorial(30))
     print(factorial.__doc__)
+    print(type(factorial))

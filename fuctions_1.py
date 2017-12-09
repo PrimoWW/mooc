2c0e9941eaf3b40e049c569944fe059278ec2d7a
PrimoWW <992462276@qq.com>
2017-12-09 13:00:19 +0800

    增加map使用实例

--- fuctions_1.py
+++ fuctions_1.py
@@ -1,11 +1,15 @@
 """
 创建并测试一个函数，然后读取__doc__属性，在检查他的类型
 """
-def factorial(n):
-    """this function is used to find n**(n-1)**(n-2)**...**1"""
-    return 1 if n==1 else n * factorial(n-1)
 
-if __name__=="__main__":
+
+def factorial(n):
+    """this function is used to return n！"""
+    return 1 if n <= 1 else n * factorial(n-1)
+
+
+if __name__ == "__main__":
     print(factorial(30))
     print(factorial.__doc__)
     print(type(factorial))
+    print(list(map(factorial, range(11))))  # 函数的特性：可以作为参数传给其他函数

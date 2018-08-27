bdec3073b0572a71becfa9a757aab1e35af924a2
PrimoWW <992462276@qq.com>
2018-08-27 16:48:00 +0800

    这道题的提面试给两个数，end和target,从1,2,3,..end中找到所有和等于target的组合 动态规划思想

---
+++ test/find_all_lis.py
@@ -0,0 +1,21 @@
+import copy
+end, target = 5, 5
+def good(start, end, target, li):
+
+    if target == 0:
+        li.sort()
+        print(' '.join(li))
+        return
+    if target < 0 or start > end:
+        return
+
+
+    li1 = copy.deepcopy(li)
+    li2 = copy.deepcopy(li)
+
+    li1.append(str(start))
+
+    good(start+1, end, target-start, li1)
+    good(start+1, end, target, li2)
+li = []
+good(1, end, target, li)
